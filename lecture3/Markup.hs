2e96d991b13ffa1d009dbec5990f4c37dbf103d1
Jan Schill <janschill@me.com>
2018-10-15 14:46:10 +0200

    Refactor XML structure to more tree like structure

--- lecture3/Markup.hs
+++ lecture3/Markup.hs
@@ -1,40 +1,57 @@
 module Markup where
 
-type TextNode = String
+data Tree a = Tree a [Tree a]
+type Name = String
 type Attribute = (String, String)
-data Tag = Tag String [Attribute] TextNode XML
-  deriving Show
-type XML = [Tag]
+data Tag = Pair Name [Attribute] [Xml] | Empty Name [Attribute]
+data XmlNode = Element Tag | TextNode String
+type Xml = Tree XmlNode
 
 attributeToString :: Attribute -> String
 attributeToString (name, value) = " " ++ name ++ "=\"" ++ value ++ "\""
 
 tagToString :: Tag -> String
-tagToString (Tag name attributes textNode xml) =
-  "<" ++ name ++ (foldr (++) "" (map attributeToString attributes)) ++ ">" ++
-  textNode ++ (xmlToString xml) ++
+tagToString (Empty name attributes) =
+  "<" ++ name ++ (concat (map attributeToString attributes)) ++ "/>"
+tagToString (Pair name attributes xml) =
+  "<" ++ name ++ (concat (map attributeToString attributes)) ++ ">" ++
+  (concat (map xmlToString xml)) ++
   "</" ++ name ++ ">"
 
-xmlToString :: XML -> String
-xmlToString xml = foldr (++) "" (map tagToString xml)
+xmlToString :: Xml -> String
+xmlToString (Tree (Element tag) trees) = tagToString tag ++ (concat (map xmlToString trees))
+xmlToString (Tree (TextNode text) trees) = text ++ (concat (map xmlToString trees))
 
-h1 :: Tag
-h1 = Tag "h1" [] "A heading" []
+newPairTag :: Name -> [Attribute] -> [Xml] -> Tag
+newPairTag name attributes children = Pair name attributes children
 
-p :: Tag
-p = Tag "p" [] "A paragraph" []
+newEmptyTag :: Name -> [Attribute] -> Tag
+newEmptyTag name attributes = Empty name attributes
 
-body :: Tag
-body = Tag "body" [("class", "content"), ("id", "main")] "" [h1, p]
+newH1 :: [Attribute] -> [Xml] -> Tag
+newH1 attributes children = newPairTag "h1" attributes children
 
-title :: Tag
-title = Tag "title" [] "Page Title" []
+newP :: [Attribute] -> [Xml] -> Tag
+newP attributes children = newPairTag "p" attributes children
 
-header :: Tag
-header = Tag "head" [] "" [title]
+newBody :: [Attribute] -> [Xml] -> Tag
+newBody attributes children = newPairTag "body" attributes children
 
-html :: Tag
-html = Tag "html" [] "" [header, body]
+newTitle :: [Attribute] -> [Xml] -> Tag
+newTitle attributes children = newPairTag "title" attributes children
 
-document :: XML
-document = [html]
+newHead :: [Attribute] -> [Xml] -> Tag
+newHead attributes children = newPairTag "head" attributes children
+
+newHtml :: [Attribute] -> [Xml] -> Tag
+newHtml attributes children = newPairTag "html" attributes children
+
+title = newTitle [] [Tree (TextNode "Page Title") []]
+h1 = newH1 [] [Tree (TextNode "A Heading") []]
+p = newP [] [Tree (TextNode "A paragraph") []]
+header = newHead [] [Tree (Element title) []]
+body = newBody [("class", "content"), ("id", "main")] [Tree (Element h1) [], Tree (Element p) []]
+html = newHtml [] [Tree (Element header) [], Tree (Element body) []]
+
+example :: Xml
+example = Tree (Element html) []

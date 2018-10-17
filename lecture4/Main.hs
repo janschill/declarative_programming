00b2135f7327d51f7b6553bc548b9e981c84856f
Jan <janschill@me.com>
2018-10-17 22:16:15 +0200

    Test boxes

--- lecture4/Main.hs
+++ lecture4/Main.hs
@@ -14,5 +14,12 @@ g3 = ((|||) g1 g2)
 g3SVG = toSVG g2
 g3XML = xmlToString g3SVG
 
+boxes = [
+  Box (Point 0 0) (Point 50 50),
+  Box (Point 150 200) (Point 400 200),
+  Nil,
+  Box (Point 300 (-200)) (Point (-100) 500)
+  ]
+
 main::IO ()
 main = writeFile "test.svg" (xmlToString g3SVG)

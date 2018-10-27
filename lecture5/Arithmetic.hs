c7285a4501b76a83a4b6ce260206c119478b7f58
Jan Schill <janschill@me.com>
2018-10-27 12:45:49 +0200

    Use Either data to throw own exception when div _ 0

--- lecture5/Arithmetic.hs
+++ lecture5/Arithmetic.hs
@@ -4,9 +4,13 @@ data BinOp = Add | Sub | Mul | Div
 data Expr
   = BinApp BinOp Expr Expr
   | Num Int
+type Error = [String]
 
 expr1 :: Expr
 expr1 = BinApp Add (BinApp Mul (Num 2) (Num 3)) (Num 4)
+
+expr2 :: Expr
+expr2 = BinApp Add (BinApp Div (Num 2) (Num 0)) (Num 4)
 
 showExpr :: Expr -> String
 showExpr (Num num) = show num
@@ -29,36 +33,6 @@ applyOp Sub x y = x - y
 applyOp Mul x y = x * y
 applyOp Div x y = div x y
 
-safeAdd :: Int -> Int -> Maybe Int
-safeAdd x y = Just (x + y)
-
-safeSub :: Int -> Int -> Maybe Int
-safeSub x y = Just (x - y)
-
-safeMul :: Int -> Int -> Maybe Int
-safeMul x y = Just (x * y)
-
-safeDiv :: Int -> Int -> Maybe Int
-safeDiv x 0 = Nothing
-safeDiv x y = Just (x `div` y)
-
-andThen :: Maybe a -> (a -> Maybe b) -> Maybe b
-andThen Nothing _ = Nothing
-andThen (Just x) f = f x
-
-safeEval' :: Expr -> Maybe Int
-safeEval' (Num num) = Just num
-safeEval' binApp =
-  case binApp of
-    BinApp Add expr1 expr2 -> eval_ expr1 expr2 safeAdd
-    BinApp Sub expr1 expr2 -> eval_ expr1 expr2 safeSub
-    BinApp Mul expr1 expr2 -> eval_ expr1 expr2 safeMul
-    BinApp Div expr1 expr2 -> eval_ expr1 expr2 safeDiv
-    where
-      eval_ :: Expr -> Expr -> (Int -> Int -> Maybe b) -> Maybe b
-      eval_ (Num num) expr f = safeEval' expr `andThen` f num
-      eval_ expr (Num num) f = safeEval' expr `andThen` f num
-
 safeEval :: Expr -> Maybe Int
 safeEval (Num n) = Just n
 safeEval (BinApp Div e1 e2) =
@@ -75,12 +49,51 @@ safeEval (BinApp op e1 e2) =
       Nothing -> Nothing
       Just x2 -> Just (applyOp op x1 x2)
 
-safeEval'' :: Expr -> Maybe Int
-safeEval'' (Num n) = Just n
-safeEval'' (BinApp op e1 e2) =
-  safeEval'' e1 `andThen` (\x1 ->
-  safeEval'' e2 `andThen` (\x2 ->
-  Just (applyOp op x1 x2)))
+andThen :: Maybe a -> (a -> Maybe b) -> Maybe b
+andThen Nothing _ = Nothing
+andThen (Just x) f = f x
+
+safeEval' :: Expr -> Maybe Int
+safeEval' (Num n) = Just n
+safeEval' (BinApp op e1 e2) =
+  safeEval' e1 `andThen` (\x1 ->
+  safeEval' e2 `andThen` (\x2 ->
+    case op of
+      Div -> case x2 of
+          0 -> Nothing
+          _ -> Just (applyOp Div x1 x2)
+      _ -> Just (applyOp op x1 x2)))
+
+eval1 :: Expr -> Either Error Int
+eval1 (Num n) = Right (n)
+eval1 (BinApp Div e1 e2) =
+  case (eval1 e2) of
+    Left error -> Left error
+    Right 0 -> Left ["Exception: divide by zero"]
+    Right x1 -> case (eval1 e1) of
+      Left error -> Left error
+      Right x2 -> Right (applyOp Div x1 x2)
+eval1 (BinApp op e1 e2) =
+  case (eval1 e1) of
+  Left error -> Left error
+  Right x1 -> case (eval1 e2) of
+    Left error -> Left error
+    Right x2 -> Right (applyOp op x1 x2)
+
+andThen' :: Either c a -> (a -> Either c b) -> Either c b
+andThen' (Left x) f = Left x
+andThen' (Right x) f = f x
+
+eval2 :: Expr -> Either Error Int
+eval2 (Num n) = Right (n)
+eval2 (BinApp op e1 e2) =
+  eval2 e1 `andThen'` (\x1 ->
+  eval2 e2 `andThen'` (\x2 ->
+    case op of
+      Div -> case x2 of
+          0 -> Left ["Exception: divide by zero"]
+          _ -> Right (applyOp Div x1 x2)
+      _ -> Right (applyOp op x1 x2)))
 
 -- map (maybeMap (+1)) [Just 5,Just 1] -> [Just 6,Just 2]
 maybeMap :: (a -> b) -> Maybe a -> Maybe b

8e2adb4e3cc68a71bda43e390ca88cbbb16b3c34
Jan Schill <janschill@me.com>
2018-10-30 13:01:18 +0100

    Better examples

--- lecture5/Parameterization.hs
+++ lecture5/Parameterization.hs
@@ -1,13 +1,13 @@
 module Parameterization where
 
-import Prelude hiding (id, const, flip, IO, length)
+import Prelude hiding (id, const, flip, IO, length, compare)
 
 id :: a -> a
 id x = x
 -- id = \x -> x
 
 const :: a -> b -> a
-const x y = x
+const x _ = x
 -- const x = \y -> x
 -- const = \x -> \y -> x
 
@@ -18,14 +18,32 @@ on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
 on f g x y = f (g x) (g y)
 
 -- Examples
+-- id
+countTrues :: [Bool] -> Int
+countTrues bs = length (filter id bs)
+
+countFalses :: [Bool] -> Int
+countFalses bs = length (filter not bs)
 
 -- const
 length :: [a] -> Int
 length = foldr (const (1+)) 0
 
+sameLength :: [Double] -> [Int]
+sameLength = map (const 23)
+
+extend :: [Double] -> [(Double, Int)]
+extend ds = zip ds (sameLength ds)
+
 -- flip
 divideInverse :: Int -> Int -> Int
 divideInverse = flip div
+
+powers :: [Int] -> [Int]
+powers xs = map ((^) 2) xs
+
+squares :: [Int] -> [Int]
+squares xs = map (flip (^) 2) xs
 
 -- on
 data IO = I | O
@@ -35,7 +53,5 @@ ioToInt :: IO -> Int
 ioToInt O = 0
 ioToInt I = 1
 
-sumToString :: Int -> Int -> String
-sumToString x y = show (x + y)
-
-exampleOn = on sumToString ioToInt I I
+compare :: IO -> IO -> Bool
+compare = on (==) ioToInt

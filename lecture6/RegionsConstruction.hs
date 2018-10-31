a75642368bbf7093a312c75527d5534510b5b95e
Jan Schill <janschill@me.com>
2018-10-31 16:21:19 +0100

    Know your geometry

--- lecture6/RegionsConstruction.hs
+++ lecture6/RegionsConstruction.hs
@@ -2,12 +2,23 @@ module RegionsConstruction where
 
 import RegionsBasics
 
+p1 :: Point
+p1 = Point 0 0
+p2 :: Point
+p2 = Point 6 6
+r1 :: Region
+r1 = rectangle 5 5
+r2 :: Region
+r2 = rectangle 10 10
+rs :: [Region]
+rs = [r1, r2]
+
+pyth :: Point -> Point -> Float
+pyth (Point xp yp) (Point xc yc) = sqrt (((xp - xc) ^ 2) + ((yp - yc) ^ 2))
+
+-- nicht richtig
 circle :: Float -> Region
-circle r = \(Point x y) ->
-  x <=  r &&
-  y <=  r &&
-  x >= -r &&
-  y >= -r
+circle r = \p -> (pyth p (Point 0 0)) <= r
 
 half :: Float -> Float
 half n = n / 2
@@ -17,7 +28,7 @@ rectangle w h = \(Point x y) ->
   x <=  (half w) &&
   x >= -(half w) &&
   y <=  (half h) &&
-  y >= -(half y)
+  y >= -(half h)
 
 outside :: Region -> Region
 -- outside r = \p -> not (r p)
